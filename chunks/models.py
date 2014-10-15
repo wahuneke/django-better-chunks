daa6cb0c3317c751485084888c60e698777c7b20
Bill Huneke <wahuneke@gmail.com>
2014-10-15 15:11:00 -0400

    * factor out Chunk lookup from template tags, put it in the model (so we can use chunks in places other than templates)
    * add some formatting for PEP8 compliance

--- chunks/models.py
+++ chunks/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.sites.models import Site
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django import template
+from django.core.cache import cache
+
 
 class Chunk(models.Model):
     """
@@ -10,13 +13,51 @@ class Chunk(models.Model):
     any template with the use of a special template
     tag
     """
+    CACHE_PREFIX = "chunk_"
+    LANG_CODE_VAR = template.Variable('LANGUAGE_CODE')
+
     key = models.CharField(help_text="A unique name for this chunk of content", blank=False, max_length=255)
     content = models.TextField(blank=True)
-    lang_code = models.CharField(verbose_name=_(u"language"), help_text="Language code, if this chunk is translated. Same format as LANGUAGE_CODE setting, e.g. sv-se, or de-de, etc.", blank=True, max_length=5, default=settings.LANGUAGE_CODE)
+    lang_code = models.CharField(verbose_name=_(u"language"), default=settings.LANGUAGE_CODE,
+                                 help_text="Language code, if this chunk is translated. Same format as LANGUAGE_CODE "
+                                           "setting, e.g. sv-se, or de-de, etc.", blank=True, max_length=5)
     site = models.ForeignKey(Site, default=settings.SITE_ID, blank=True, null=True, verbose_name=_('site'))
 
-    class Meta:
+    class Meta(object):
         unique_together = (('key', 'lang_code', 'site'),)
+
+    @classmethod
+    def get_chunk(cls, name, context=None, cache_time=0):
+        """
+        Get the named string chunk
+        :param name: name of chunk to retrieve
+        :param context: request context (if available) so we can get language code
+        :param cache_time: number of seconds this value can be cached
+        """
+
+        if context is None:
+            lang = settings.LANGUAGE_CODE
+        else:
+            try:
+                lang = cls.LANG_CODE_VAR.resolve(context)
+            except template.VariableDoesNotExist:
+                # no LANGUAGE_CODE variable found in context, just return ''
+                return ''
+
+        site = Site.objects.get_current().id  # Django caches get_current()
+        cache_key = cls.CACHE_PREFIX + name + lang + str(site)
+        content = cache.get(cache_key)
+        if content is None:
+            try:
+                chunk = cls.objects.get(key=name, lang_code=lang, site=site)
+                content = chunk.content
+            except Chunk.DoesNotExist:
+                # cache missing models as empty chunk strings
+                content = ''
+            if cache_time > 0:
+                # don't even call cache if timeout is 0
+                cache.set(cache_key, content, cache_time)
+        return content
 
     def __unicode__(self):
         return u"%s" % (self.key,)
--- chunks/templatetags/chunks.py
+++ chunks/templatetags/chunks.py
@@ -1,54 +1,36 @@
 from django import template
 from django.db import models
-from django.core.cache import cache
-from django.contrib.sites.models import Site
 
 register = template.Library()
 
 Chunk = models.get_model('chunks', 'Chunk')
-CACHE_PREFIX = "chunk_"
+
 
 def do_get_chunk(parser, token):
     # split_contents() knows not to split quoted strings.
     tokens = token.split_contents()
-    if len(tokens) < 2 or len(tokens) > 3:
-        raise template.TemplateSyntaxError, "%r tag should have either 2 or 3 arguments" % (tokens[0],)
     if len(tokens) == 2:
         tag_name, key = tokens
         cache_time = 0
-    if len(tokens) == 3:
+    elif len(tokens) == 3:
         tag_name, key, cache_time = tokens
+    else:
+        raise template.TemplateSyntaxError, "%r tag should have either 2 or 3 arguments" % (tokens[0],)
+
     # Check to see if the key is properly double/single quoted
     if not (key[0] == key[-1] and key[0] in ('"', "'")):
         raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
     # Send key (without quotes) and caching time
     return ChunkNode(key[1:-1], cache_time)
     
+
 class ChunkNode(template.Node):
     def __init__(self, key, cache_time=0):
        self.key = key
        self.cache_time = int(cache_time)
-       self.lang_code = template.Variable('LANGUAGE_CODE')
-    
+
     def render(self, context):
-        try:
-            lang = self.lang_code.resolve(context)
-        except template.VariableDoesNotExist:
-            # no LANGUAGE_CODE variable found in context, just return ''
-            return ''
-        site = Site.objects.get_current().id  # Django caches get_current()
-        cache_key = CACHE_PREFIX + self.key + lang + str(site)
-        content = cache.get(cache_key)
-        if content is None:
-            try:
-                chunk = Chunk.objects.get(key=self.key, lang_code=lang, site=site)
-                content = chunk.content
-            except Chunk.DoesNotExist:
-                # cache missing models as empty chunk strings
-                content = ''
-            if self.cache_time > 0: 
-                # don't even call cache if timeout is 0
-                cache.set(cache_key, content, self.cache_time)
-        return content
+        return Chunk.get_chunk(self.key, context, self.cache_time)
+
 
 register.tag('chunk', do_get_chunk)
