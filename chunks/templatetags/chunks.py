72dc398e2971804c013d8feaacd9e97820bdc646
Bill Huneke <wahuneke@gmail.com>
2014-10-15 16:43:00 -0400

    make chunk tag work as assignment tag too

--- chunks/templatetags/chunks.py
+++ chunks/templatetags/chunks.py
@@ -9,11 +9,23 @@ Chunk = models.get_model('chunks', 'Chunk')
 def do_get_chunk(parser, token):
     # split_contents() knows not to split quoted strings.
     tokens = token.split_contents()
-    if len(tokens) == 2:
+    cnt = len(tokens)
+    if cnt == 2:
+        # Called with just a chunk key
         tag_name, key = tokens
         cache_time = 0
-    elif len(tokens) == 3:
+        output_variable = None
+    elif cnt == 3:
+        # Called with chunk key and cache time
         tag_name, key, cache_time = tokens
+        output_variable = None
+    elif cnt == 4 and tokens[-2] == 'as':
+        # Called with variable assignment but no cache time
+        tag_name, key, _as, output_variable = tokens
+        cache_time = 0
+    elif cnt == 5 and tokens[-2] == 'as':
+        # Called with variable assignment and cache time
+        tag_name, key, cache_time, _as, output_variable = tokens
     else:
         raise template.TemplateSyntaxError, "%r tag should have either 2 or 3 arguments" % (tokens[0],)
 
@@ -21,16 +33,21 @@ def do_get_chunk(parser, token):
     if not (key[0] == key[-1] and key[0] in ('"', "'")):
         raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
     # Send key (without quotes) and caching time
-    return ChunkNode(key[1:-1], cache_time)
+    return ChunkNode(key[1:-1], cache_time, output_variable)
     
 
 class ChunkNode(template.Node):
-    def __init__(self, key, cache_time=0):
-       self.key = key
-       self.cache_time = int(cache_time)
+    def __init__(self, key, cache_time=0, output_variable=None):
+        self.key = key
+        self.output_variable = output_variable
+        self.cache_time = int(cache_time)
 
     def render(self, context):
-        return Chunk.get_chunk(self.key, context, self.cache_time)
-
+        content = Chunk.get_chunk(self.key, context, self.cache_time)
+        if self.output_variable is None:
+            return content
+        else:
+            context[self.output_variable] = content
+            return ''
 
 register.tag('chunk', do_get_chunk)
